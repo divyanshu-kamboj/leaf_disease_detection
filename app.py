9a6a6feffb4de286ab3f24e62a7845440e7e5483
Divyanshu Thind <100478745+divyanshu-kamboj@users.noreply.github.com>
2025-02-09 08:14:50 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+import gdown
+import os
+
+
+file_id = "1TAJs8KelUm-XHE6IUyjlWHUjYm3FTY6o"
+model_path = "trained_plant_disease_model.keras"
+
+if not os.path.exists(model_path):
+    st.warning("Downloading model from Google Drive...")
+    
+    # Correct Google Drive download link format
+    gdrive_url = f"https://drive.google.com/uc?id={file_id}"
+    
+    # Download the model
+    gdown.download(gdrive_url, model_path, quiet=False)
+    
+    # Check if download was successful
+    if os.path.exists(model_path):
+        st.success("Model downloaded successfully!")
+    else:
+        st.error("Failed to download the model.")   
+
+st.logo("logo.jpeg")
+def model_prediction(test_image):
+    model= tf.keras.models.load_model("trained_plant_disease_model.keras")
+    image= tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr=np.array([input_arr])
+    predictions= model.predict(input_arr)
+    return np.argmax(predictions)
+
+st.sidebar.title("Plant Disease system for Sustainable Agriculture")
+app_mode = st.sidebar.selectbox('select page',['Home','Disease Recognition'])
+
+from PIL import Image
+img= Image.open('Patoto.jpeg')
+st.image(img)
+
+if(app_mode=='HOME'):
+    st.markdown("<h1 style='text-align: center;'>Plant Disease Detection System for Sustainable Agriculture", unsafe_allow_html=True)
+
+elif(app_mode=='Disease Recognition'):
+    st.header('Plant Disease Detection System For Sustainable Agriculture')
+
+
+test_image= st.file_uploader('Choose an image:')
+if(st.button('Show Image')):
+    st.image(test_image,width=4,use_column_width=True)
+
+if (st.button('Predict')):
+    st.snow()
+    st.write('our prediction')
+    result_index = model_prediction(test_image)
+    class_name=['Potato___Early_blight','Potato___Late_blight','Potato___healthy']
+    st.success('Model is predicting its a {}'.format(class_name[result_index]))
